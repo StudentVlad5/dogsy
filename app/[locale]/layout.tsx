ef101735cd4ed6d800e63f1997da7b60bc6654e1
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-07-18 19:45:43 +0300

    start project

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { type Locale, locales } from "@/i18n.config";
+import { unstable_setRequestLocale } from "next-intl/server";
+import {Providers} from './providers';
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js Weather",
+  description:
+    "A weather app built with Next.js and next-intl",
+};
+
+ export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+ }
+
+export default function LocaleLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: Locale };
+}>) {
+  unstable_setRequestLocale(locale);
+  return (
+    <html lang={locale}>
+      <body className="mx-auto w-[500px] bg-slate-950 px-6 pt-3 text-sky-100">
+
+            <Providers>
+
+              {children}
+            </Providers>
+
+      </body>
+    </html>
+  );
+}
